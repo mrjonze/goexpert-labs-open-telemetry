49eb5c568821fb1846c0ea3f4cd837999a298689
Luis Correia Lima Neto <mr.jonze@gmail.com>
2024-05-09 17:08:45 -0300

    alterando o endpoint para post

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"encoding/json"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"unicode"
+)
+
+type ViaCep struct {
+	Localidade string `json:"localidade"`
+	Erro       bool   `json:"erro"`
+}
+
+type WeatherApi struct {
+	Current struct {
+		TempC float64 `json:"temp_c"`
+		TempF float64 `json:"temp_f"`
+		TempK float64 `json:"temp_k"`
+	} `json:"current"`
+}
+
+var postData struct {
+	Cep string `json:"cep"`
+}
+
+func main() {
+	http.HandleFunc("/", SearchCepHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func SearchCepHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error reading request body"))
+		return
+	}
+
+	err = json.Unmarshal(body, &postData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error unmarshalling request body"))
+		return
+	}
+
+	if postData.Cep == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("cep parameter is required"))
+		return
+	}
+
+	validate := regexp.MustCompile(`^[0-9]{8}$`)
+	if !validate.MatchString(postData.Cep) {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("invalid zipcode"))
+		return
+	}
+
+	cep, err := SearchCep(postData.Cep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		errorStr := err.Error()
+		w.Write([]byte("error while searching for cep: " + errorStr))
+		return
+	}
+
+	if cep.Erro {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("can not find zipcode"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	weather, err := SearchTemperature(cep.Localidade)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		errorStr := err.Error()
+		w.Write([]byte("error while searching for temperature: " + errorStr))
+		return
+	}
+	if weather != nil {
+		json.NewEncoder(w).Encode(weather.Current)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("can not find temperature"))
+	}
+}
+
+func SearchCep(cep string) (*ViaCep, error) {
+	req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data ViaCep
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func SearchTemperature(city string) (*WeatherApi, error) {
+	req, err := http.Get("http://api.weatherapi.com/v1/current.json?key=148a907896384b7b89f232427240605&aqi=no&q=" + removeDiacriticsAndEncodeCityName(city))
+
+	if err != nil {
+		return nil, err
+	}
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data WeatherApi
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	data.Current.TempF = data.Current.TempC*1.8 + 32
+	data.Current.TempK = data.Current.TempC + 273
+
+	return &data, nil
+}
+
+func isMn(r rune) bool {
+	return unicode.Is(unicode.Mn, r)
+}
+
+func removeDiacriticsAndEncodeCityName(s string) string {
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+	result, _, _ := transform.String(t, s)
+	result = url.QueryEscape(result)
+	println(result)
+	return result
+}
